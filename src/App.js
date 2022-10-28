da2d67a207a955a376c7fec35ae18999a39605a0
mythily-muthu <mythilimuthu18@gmail.com>
2022-10-28 14:51:46 +0530

    form validation added

--- src/App.js
+++ src/App.js
@@ -1,99 +1,123 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-function App() {
+function App()
 
+let initialValues = {
+  username: "",
+  email: "",
+  password: ""
+};
 
+let [formValues, setformValues] = useState(initialValues);
+let [formErrors, setformErrors] = useState({});
+let [isSubmit, setisSubmit] = useState(false);
 
+let handleChange = (e) => {
 
-  let initialValues = {
+  let { name, value } = e.target; //destructuring
+  setformValues({ ...formValues, [e.target.name]: e.target.value })
+
+}
+
+let handleSubmit = (e) => {
+  e.preventDefault();
+  console.log("output:", formValues);
+  setformValues({
+    ...formValues,
     username: "",
     email: "",
     password: ""
   };
+  setformErrors(validate(formValues));
+  setisSubmit(true);
 
-  let [formValues, setformValues] = useState(initialValues)
+}
+useEffect(() => {
+  console.log(formErrors)
+  if (Object.keys(formErrors).length === 0 && isSubmit) {
+    console.log(formValues);
+  }
+}, [formErrors]);
 
-  let handleChange = (e) => {
 
-    let { name, value } = e.target; //destructuring
-    setformValues({ ...formValues, [e.target.name]: e.target.value })
+let validate = (values) => {
+  let errors = {};
+  let regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
 
+  if (!values.username) {
+    errors.username = "Username is required";
+  }
+  if (!values.email) {
+    errors.email = "Email is required";
   }
 
-  let handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("output:", formValues);
-    setformValues({
-      ...formValues,
-      username: "",
-      email: "",
-      password: ""
-    })
+  if (!value.password) {
+    errors.password = "Password is required";
   }
+  return errors;
+};
+
+return (
+  <div className="container">
+
+    <form onSubmit={handleSubmit}>
+
+      <h1>Login Form</h1>
+
+      <div className='ui divider'></div>
+      <div className='ui form'>
 
 
-
-
-  return (
-    <div className="container">
-
-      <form >
-
-        <h1>Login Form</h1>
-
-        <div className='ui divider'></div>
-        <div className='ui form'>
-
-
-          <div className='field'>
-            <label>Username</label>
-            <input
-              type="text"
-              name="username"
-              placeholder='Username'
-              value={formValues.username}
-              onChange={(e) => {
-                setformValues({ ...formValues, username: e.target.value })
-              }}
-            />
-          </div>
-
-          <div className='field'>
-            <label>Email</label>
-            <input
-              type="email"
-              name="email"
-              placeholder='Email'
-              value={formValues.email}
-              onChange={(e) => {
-                setformValues({ ...formValues, [e.target.name]: e.target.value })
-              }
-              }
-            />
-          </div>
-
-          <div className='field'>
-            <label>Password</label>
-            <input
-              type="password"
-              name="password"
-              placeholder='Password'
-              value={formValues.password}
-              onChange={handleChange}
-            />
-          </div>
-
-          <button
-            onClick={handleSubmit}
-            className='fluid ui buttton blue'
-          >Submit</button>
-
+        <div className='field'>
+          <label>Username</label>
+          <input
+            type="text"
+            name="username"
+            placeholder='Username'
+            value={formValues.username}
+            onChange={(e) => {
+              setformValues({ ...formValues, username: e.target.value })
+            }}
+          />
         </div>
-      </form>
-    </div>
-  );
+        <p>{formErrors.username}</p>
+        <div className='field'>
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder='Email'
+            value={formValues.email}
+            onChange={(e) => {
+              setformValues({ ...formValues, [e.target.name]: e.target.value })
+            }
+            }
+          />
+        </div>
+        <p>{formErrors.email}</p>
+
+        <div className='field'>
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder='Password'
+            value={formValues.password}
+            onChange={handleChange}
+          />
+        </div>
+        <p>{formErrors.password}</p>
+        <button
+          onClick={handleSubmit}
+          className='fluid ui buttton blue'
+        >Submit</button>
+
+      </div>
+    </form>
+  </div>
+);
 }
 
 export default App;
